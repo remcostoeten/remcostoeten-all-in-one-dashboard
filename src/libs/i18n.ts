9d28df121815bef8836405b7cc0184927d863840
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-30 20:31:40 +0200

    work on some refactoring to my liking

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -6,7 +6,7 @@ import { getRequestConfig } from 'next-intl/server';
 
 // The localisation files are synced with Crowdin using GitHub Actions.
 // By default, there are 3 ways to sync the message files:
-// 1. Automatically sync on push to the `main` branch
+// 1. Automatically sync on push to the `master` branch
 // 2. Run manually the workflow on GitHub Actions
 // 3. Every 24 hours at 5am, the workflow will run automatically
 
