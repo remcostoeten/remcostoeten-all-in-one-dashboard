6d1967d797009852f5ceca72fe9535d0f65cd8c6
Remco Stoeten <remcostoeten@hotmail.com>
2024-05-31 21:52:19 +0200

    style(run formatter): run formatter and remove lock

--- src/libs/i18nNavigation.ts
+++ src/libs/i18nNavigation.ts
@@ -1,6 +1,6 @@
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
-import { AppConfig } from '@/utils/AppConfig';
+import { AppConfig } from "@/utils/AppConfig";
 
 export const { usePathname, useRouter } = createSharedPathnamesNavigation({
   locales: AppConfig.locales,
