f26eadb60d1b79307d8a875c05c8e556de38f257
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-01 01:07:20 +0200

    restructure for future bigger schemas

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,13 +2,13 @@ import type { Config } from 'drizzle-kit';
 
 /** @type {import('drizzle-kit').Config} */
 export default {
-  driver: 'turso',
-  out: './migrations',
-  schema: './src/models/Schema.ts',
-  dialect: 'sqlite',
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-    authToken: process.env.DATABASE_AUTH_TOKEN,
-  },
-  verbose: true,
+    driver: 'turso',
+    out: './migrations',
+    schema: './src/models/index.ts',
+    dialect: 'sqlite',
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+        authToken: process.env.DATABASE_AUTH_TOKEN,
+    },
+    verbose: true,
 } satisfies Config;
---
+++ src/models/index.ts
@@ -0,0 +1,3 @@
+import { guestbookSchema } from './guestbook';
+
+export { guestbookSchema };
