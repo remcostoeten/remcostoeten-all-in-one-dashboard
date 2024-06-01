5a2b8f742d2ece4c16e610b45afd4a2b629a14a9
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-01 15:05:38 +0200

    Add taskschema

--- src/models/Schema.ts
+++ src/models/Schema.ts
@@ -12,3 +12,16 @@ export const guestbookSchema = sqliteTable('guestbook', {
     sql`(strftime('%s', 'now'))`,
   ),
 });
+
+export const taskSchema = sqliteTable('task', {
+  id: integer('id').primaryKey(),
+  title: text('title').notNull(),
+  description: text('description'),
+  completed: integer('completed').notNull().default(0), // Default to 0 (false)
+  createdAt: integer('created_at', { mode: 'timestamp' }).default(
+    sql`(strftime('%s', 'now'))`,
+  ),
+  updatedAt: integer('updated_at', { mode: 'timestamp' }).default(
+    sql`(strftime('%s', 'now'))`,
+  ),
+});
