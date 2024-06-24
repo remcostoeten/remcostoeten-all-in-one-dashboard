8de6758f647a6844ab36d04628f400e7b51091b7
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-24 17:30:27 +0200

    fix: clean up types and eslint errors

--- src/components/shared/navigation/Search.tsx
+++ src/components/shared/navigation/Search.tsx
@@ -4,16 +4,13 @@ import React, { useState, useEffect, useRef } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
-import { MicrophoneIcon } from '@heroicons/react/24/outline'
-import { CameraIcon, ArchiveIcon } from '@radix-ui/react-icons'
-import { searchIcon, MarkReadIcon, MarkUnreadIcon, chevronIcons } from './search/SearchIcons'
 
 type CommandItemProps = {
     icon: React.ReactNode
     text: string
     shortcut?: string
 }
-function CommandItem({ icon, text, shortcut }: CommandItemProps) {
+
 const CommandItem: React.FC<CommandItemProps> = ({ icon, text, shortcut }) => (
     <div className='flex flex-col justify-center px-3.5 py-1 w-full max-md:max-w-full'>
         <div className='hover:cursor-pointer transition-all duration-200 hover:bg-icon-active-background flex justify-between gap-0 p-1.5 max-md:flex-wrap max-md:max-w-full rounded-md h-[40px] px-3.5'>
@@ -93,8 +90,8 @@ function SearchDialog() {
             }
             if (event.ctrlKey && event.key === 'm') {
                 event.preventDefault()
+                console.log('Microphone')
             }
-
             if (event.key === 'Escape') {
                 setIsOpen(false)
             }
@@ -206,7 +203,7 @@ function SearchDialog() {
                             </section>
                             <footer className='flex flex-col justify-center px-5 py-px  w-full border-t border-solid border-white border-opacity-10 max-md:mt-10 max-md:max-w-full'>
                                 <nav className='flex gap-0 py-2.5 pr-20 max-md:flex-wrap max-md:pr-5'>
-                                    <chevronIcons/>
+                                    {chevronIcons()}
                                     <span className='justify-center self-start px-2.5 text-xs whitespace-nowrap text-neutral-400'>
                                         Type
                                     </span>
@@ -215,15 +212,380 @@ function SearchDialog() {
                                             /
                                         </div>
                                     </div>
-                                    <span className='justify-center self-start px-2.5 text-xs text-neutral-400'>
+                                    <span className='justify-center self-start px-2.5 text-xs whitespace-nowrap text-neutral-400'>
                                         to view available commands
                                     </span>
                                 </nav>
                             </footer>
-                        (</motion.main>
+                        </motion.main>
                     </motion.div>
                 )}
             </AnimatePresence>
         </>
+    )
+}
+
+export default SearchDialog
+
+function chevronIcons() {
+    return (
+        <>
+            <svg
+                width='15'
+                height='15'
+                viewBox='0 0 15 15'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+            >
+                <path
+                    d='M7.52786 5.35144L3.15286 9.72644L3.77158 10.3452L7.52786 6.58888L11.2841 10.3452L11.9029 9.72644L7.52786 5.35144Z'
+                    fill='#8A8F98'
+                    fillOpacity='0.6'
+                />
+            </svg>
+
+            <svg
+                width='15'
+                height='15'
+                viewBox='0 0 15 15'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+            >
+                <path
+                    d='M7.16786 10.6014L2.79286 6.22639L3.40536 5.61389L7.16786 9.37639L10.9304 5.61389L11.5429 6.22639L7.16786 10.6014Z'
+                    fill='#8A8F98'
+                    fillOpacity='0.6'
+                />
+            </svg>
+        </>
+    )
+}
+
+const ContactRecordIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M7 7C8.65685 7 10 5.65685 10 4C10 2.34315 8.65685 1 7 1C5.34315 1 4 2.34315 4 4C4 5.65685 5.34315 7 7 7Z'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M1 13V12C1 10.3431 2.34315 9 4 9H10C11.6569 9 13 10.3431 13 12V13'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+const DepartmentIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M11 5L7 1L3 5'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M1 9H13'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M11 9V13H3V9'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+const MicrophoneIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M7 1C6.46957 1 5.96086 1.21071 5.58579 1.58579C5.21071 1.96086 5 2.46957 5 3V7C5 7.53043 5.21071 8.03914 5.58579 8.41421C5.96086 8.78929 6.46957 9 7 9C7.53043 9 8.03914 8.78929 8.41421 8.41421C8.78929 8.03914 9 7.53043 9 7V3C9 2.46957 8.78929 1.96086 8.41421 1.58579C8.03914 1.21071 7.53043 1 7 1Z'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M13 7C13 8.5913 12.3679 10.1174 11.2426 11.2426C10.1174 12.3679 8.5913 13 7 13C5.4087 13 3.88258 12.3679 2.75736 11.2426C1.63214 10.1174 1 8.5913 1 7'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+const CameraIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M13 10L9 7L13 4V10Z'
+            stroke='white'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M1 1H8C8.53043 1 9.03914 1.21071 9.41421 1.58579C9.78929 1.96086 10 2.46957 10 3V11C10 11.5304 9.78929 12.0391 9.41421 12.4142C9.03914 12.7893 8.53043 13 8 13H1C0.734784 13 0.48043 12.8946 0.292893 12.7071C0.105357 12.5196 0 12.2652 0 12V2C0 1.73478 0.105357 1.48043 0.292893 1.29289C0.48043 1.10536 0.734784 1 1 1Z'
+            stroke='white'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+const ArchiveIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M13 5V13H1V5'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M14 1H0V5H14V1Z'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M6 8H8'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+const MarkReadIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M13 7V11C13 11.5304 12.7893 12.0391 12.4142 12.4142C12.0391 12.7893 11.5304 13 11 13H3C2.46957 13 1.96086 12.7893 1.58579 12.4142C1.21071 12.0391 1 11.5304 1 11V3C1 2.46957 1.21071 1.96086 1.58579 1.58579C1.96086 1.21071 2.46957 1 3 1H9'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M5 7L7 9L13 3'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+const MarkUnreadIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M13 7V11C13 11.5304 12.7893 12.0391 12.4142 12.4142C12.0391 12.7893 11.5304 13 11 13H3C2.46957 13 1.96086 12.7893 1.58579 12.4142C1.21071 12.0391 1 11.5304 1 11V3C1 2.46957 1.21071 1.96086 1.58579 1.58579C1.96086 1.21071 2.46957 1 3 1H9'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M10 1V7'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M13 4L10 7L7 4'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+const AddTodoIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M7 1V13'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M1 7H13'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+const NewIssueIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M7 1V13'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M1 7H13'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+const SelectAllIcon = () => (
+    <svg
+        width='14'
+        height='14'
+        viewBox='0 0 14 14'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path
+            d='M10 3H11C11.2652 3 11.5196 3.10536 11.7071 3.29289C11.8946 3.48043 12 3.73478 12 4V5'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M10 11H11C11.2652 11 11.5196 10.8946 11.7071 10.7071C11.8946 10.5196 12 10.2652 12 10V9'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M4 3H3C2.73478 3 2.48043 3.10536 2.29289 3.29289C2.10536 3.48043 2 3.73478 2 4V5'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+        <path
+            d='M4 11H3C2.73478 11 2.48043 10.8946 2.29289 10.7071C2.10536 10.5196 2 10.2652 2 10V9'
+            stroke='white'
+            strokeOpacity='0.6'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+        />
+    </svg>
+)
+
+function searchIcon() {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth={1.5}
+            stroke='currentColor'
+            width={18}
+            height={18}
+            className='opacity-60'
+        >
+            <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+            />
+        </svg>
     )
 }
