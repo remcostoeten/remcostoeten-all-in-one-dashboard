7ff9b636ea828e4fb2f5447f0e59b2e73b08da74
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-25 20:35:34 +0200

    feat: implement top bar state with zustand.

--- src/components/shared/navigation/index.ts
+++ src/components/shared/navigation/index.ts
@@ -1,8 +1,10 @@
+import ModeToggle from './ModeToggle'
+
 export { default as Breadcrumbs } from './Breadcrumbs'
 export { CurrentTime } from './CurrentTime'
 export { default as DisplayCity } from './DisplayCity'
 export { default as Logo } from './Logo'
-export { ModeToggle } from './ModeToggle'
 export { default as Search } from './Search'
 export { default as TopNavSettings } from './TopNavSettings'
 export { default as LocaleSwitcher } from './LocaleSwitcher'
+export { ModeToggle }
