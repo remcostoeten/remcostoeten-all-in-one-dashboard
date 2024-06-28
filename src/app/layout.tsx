313f451c7d57cd05296b0f39d632aa13cbf325cc
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-28 19:51:10 +0200

    clean avatar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function RootLayout({
     children
 }: {
     children: React.ReactNode
 }) {
     return (
-        <html lang='en'>
-            <body className={inter.className}>{children}</body>
-        </html>
+        <>{children}</>
     )
 }
