ede6016ee63345f861a13dbbd231e804c512c654
Remco Stoeten <remcostoeten@hotmail.com>
2024-07-12 21:03:24 +0200

    attempt to fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,50 @@
-import { authMiddleware } from '@clerk/nextjs';
-import createMiddleware from 'next-intl/middleware';
-import { NextRequest, NextResponse } from 'next/server';
-import { AppConfig } from './core/utils/app-config';
-import type createMiddleware from 'next-intl/middleware';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import type { NextFetchEvent, NextRequest } from 'next/server'
+import createMiddleware from 'next-intl/middleware'
+
+import { AppConfig } from './core/utils/app-config'
 
 const intlMiddleware = createMiddleware({
     locales: AppConfig.locales,
     localePrefix: AppConfig.localePrefix,
     defaultLocale: AppConfig.defaultLocale
-});
+})
 
-const publicRoutes = ['/sign-in(.*)', '/sign-up(.*)'];
+const isProtectedRoute = createRouteMatcher([
+    '/dashboard(.*)',
+    '/:locale/dashboard(.*)'
+])
 
-export default authMiddleware({
-    publicRoutes,
-    beforeAuth: (req) => {
-        // Run next-intl middleware before Clerk's auth middleware
-        return intlMiddleware(req);
-    },
-    afterAuth: (auth, req) => {
-        // Handle authentication logic here
-        if (!auth.userId && !auth.isPublicRoute) {
-            const locale = req.nextUrl.pathname.split('/')[1] || AppConfig.defaultLocale;
-            const signInUrl = new URL(`/${locale}/sign-in`, req.url);
-            signInUrl.searchParams.set('redirect_url', req.url);
-            return NextResponse.redirect(signInUrl);
-        }
+export default function middleware(
+    request: NextRequest,
+    event: NextFetchEvent
+) {
+    // Run Clerk middleware only when it's necessary
+    if (
+        request.nextUrl.pathname.includes('/sign-in') ||
+        request.nextUrl.pathname.includes('/sign-up') ||
+        isProtectedRoute(request)
+    ) {
+        return clerkMiddleware((auth, req) => {
+            if (isProtectedRoute(req)) {
+                const locale =
+                    req.nextUrl.pathname.match(/(\/.*)\/dashboard/)?.at(1) ?? ''
 
-        // If the user is authenticated and trying to access a public route,
-        // redirect them to the dashboard
-        if (auth.userId && auth.isPublicRoute) {
-            const locale = req.nextUrl.pathname.split('/')[1] || AppConfig.defaultLocale;
-            const dashboardUrl = new URL(`/${locale}/dashboard`, req.url);
-            return NextResponse.redirect(dashboardUrl);
-        }
+                const signInUrl = new URL(`${locale}/sign-in`, req.url)
 
-        // Allow the request to proceed
-        return NextResponse.next();
-    },
-});
+                auth().protect({
+                    // `unauthenticatedUrl` is needed to avoid error: "Unable to find `next-intl` locale because the middleware didn't run on this request"
+                    unauthenticatedUrl: signInUrl.toString()
+                })
+            }
+
+            return intlMiddleware(req)
+        })(request, event)
+    }
+
+    return intlMiddleware(request)
+}
 
 export const config = {
-    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
+}
