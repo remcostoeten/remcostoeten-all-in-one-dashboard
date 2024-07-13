2e2b171427f4698f82bbb918baf7c86662a3101c
Remco - Win laptop <remcostoeten@hotmail.com>
2024-07-13 12:43:47 +0200

    Add alot of planner files and atotempt to incorperate turb

---
+++ src/core/models/Resource.ts
@@ -0,0 +1,10 @@
+
+
+export type resourceType = 'room' | 'person' | 'equipment' | 'service' | 'other';
+
+export interface Resource {
+    id: string;
+    name: string;
+    type: resourceType;
+    details: {[key:string]:any};
+}
